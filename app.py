47389690a8de265015c21651e58315ace82e8a32
Alexandre de Sousa Machado <alexandresousamachado@hotmail.com>
2022-06-20 08:03:51 -0300

    Early 20-Jun

--- app.py
+++ app.py
@@ -1,32 +1,45 @@
-#from flask import Flask, render_template, request, redirect
-from flask_bootstrap import Bootstrap
+from datetime import datetime
+
 from flask import Flask, render_template, request, sessions
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootst = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+def render_template_anette(html_file, page_title):
+    user_agent = request.headers.get('User-Agent')
+    return render_template(html_file, page_title=page_title,
+        usuario="Alexandre", user_agent=user_agent,
+        current_time=datetime.utcnow())
+
+def render_form_anette(html_file, page_title):
+    user_agent = request.headers.get('User-Agent')
+    return render_template(html_file, page_title=page_title,
+        usuario="Alexandre", user_agent=user_agent,
+        current_time=datetime.utcnow(), form=form)
+
+def render_error(error_number, error_message):
+    return render_template("erro.html", page_title="Erro",
+        error_number=str(error_number), error_message=error_message,
+        current_time=datetime.utcnow()), error_number
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user_agent = request.headers.get('User-Agent')
-    usuario = 'Alexandre'
-    return render_template('index.html', titulo_pagina="Início", usuario=usuario, user_agent=user_agent)
+    return render_template_anette('index.html', page_title="Início")
 
-@app.route('/estagiario/')
+@app.route('/estagiarios/')
 def estagiarios():
-    user_agent = request.headers.get('User-Agent')
-    return render_template("user.html", titulo_pagina="Estagiários", usuario="Alexandre", user_agent=user_agent)
+    return render_template_anette("user.html", page_title="Estagiários")
 
 @app.route('/empresas/')
 def empresas():
-    user_agent = request.headers.get('User-Agent')
-    return render_template("user.html", titulo_pagina="Empresas", usuario="empresas", user_agent=user_agent)
+    return render_template_anette("user.html", page_title="Empresas")
 
 #@app.route('/empresa/<username>')
 #def greet(username):
 #    return f"Hi, {username}"
-
-def render_error(error_number, error_message):
-    return render_template("erro.html", titulo_pagina="Erro", error_number=str(error_number), error_message=error_message), error_number
 
 @app.errorhandler(404)
 def pagina_nao_encontrada(e):
